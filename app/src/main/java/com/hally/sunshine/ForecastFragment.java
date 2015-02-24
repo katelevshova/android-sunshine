66c650b3fcd16c5f53215d29b01b3cdef2873bf4
Kate Levshova <11220246+katelevshova@users.noreply.github.com>
2015-02-24 15:09:31 -0800

    Issue#3 - Lesson 2 - added Refresh menu item

--- app/src/main/java/com/hally/sunshine/ForecastFragment.java
+++ app/src/main/java/com/hally/sunshine/ForecastFragment.java
@@ -3,6 +3,9 @@ package com.hally.sunshine;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
@@ -27,6 +30,7 @@ public class ForecastFragment extends Fragment
 	public void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
+		setHasOptionsMenu(true);
 	}
 
 	@Override
@@ -46,7 +50,8 @@ public class ForecastFragment extends Fragment
 		List<String> weekForecastArrayList = new ArrayList<String>(Arrays.asList(forecastArray));
 
 		ArrayAdapter<String> forecastAdapter = new ArrayAdapter<String>(getActivity(),
-				R.layout.list_item_forecast, R.id.list_item_forecast_textview, weekForecastArrayList);
+				R.layout.list_item_forecast, R.id.list_item_forecast_textview,
+				weekForecastArrayList);
 
 		View rootView = inflater.inflate(R.layout.fragment_main, container, false);
 
@@ -54,8 +59,30 @@ public class ForecastFragment extends Fragment
 				.listview_forecast);
 		listViewForecast.setAdapter(forecastAdapter);
 
-		//FetchWeekWeatherTask fetchWeekWeatherTask = new FetchWeekWeatherTask();
-		//fetchWeekWeatherTask.execute();
 		return rootView;
 	}
+
+	@Override
+	public void onCreateOptionsMenu(Menu menu, MenuInflater inflater)
+	{
+		inflater.inflate(R.menu.menu_forecastfragment, menu);
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item)
+	{
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+
+		if (id == R.id.action_refresh)
+		{
+			FetchWeekWeatherTask fetchWeekWeatherTask = new FetchWeekWeatherTask();
+			fetchWeekWeatherTask.execute();
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
 }
+
