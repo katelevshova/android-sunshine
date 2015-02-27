d8ef185341c5c4fc29a2d86162b725e764b54fa0
Kate Levshova <11220246+katelevshova@users.noreply.github.com>
2015-02-26 20:13:56 -0800

    Issue#5 - Lesson 3 - removed faked data for dataAdapter

--- app/src/main/java/com/hally/sunshine/view/MainForecastFragment.java
+++ app/src/main/java/com/hally/sunshine/view/MainForecastFragment.java
@@ -19,8 +19,6 @@ import com.hally.sunshine.FetchWeekWeatherTask;
 import com.hally.sunshine.R;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 
 /**
@@ -52,21 +50,9 @@ public class MainForecastFragment extends Fragment
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 							 Bundle savedInstanceState)
 	{
-		String[] forecastArray = {
-				"Mon 6/23 - Sunny - 31/17",
-				"Tue 6/24 - Foggy - 21/8",
-				"Wed 6/25 - Cloudy - 22/17",
-				"Thurs 6/26 - Rainy - 18/11",
-				"Fri 6/27 - Foggy - 21/10",
-				"Sat 6/28 - TRAPPED IN WEATHERSTATION - 23/18",
-				"Sun 6/29 - Sunny - 20/7"
-		};
-
-		List<String> weekForecastArrayList = new ArrayList<String>(Arrays.asList(forecastArray));
-
 		_forecastAdapter = new ArrayAdapter<String>(getActivity(),
 				R.layout.list_item_forecast, R.id.list_item_forecast_textview,
-				weekForecastArrayList);
+				new ArrayList<String>());
 
 		View rootView = inflater.inflate(R.layout.fragment_main, container, false);
 
